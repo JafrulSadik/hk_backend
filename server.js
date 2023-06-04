a627f19cf9ef3b40a3ac71d119fb3fd5deb07b48
Jafrul Sadik <89705066+JafrulSadik@users.noreply.github.com>
2023-06-04 20:19:07 +0600

    Ok

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+const userRoute = require("./routes/userRoute");
+
+app.use(cors());
+
+const connect = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.use(express.json());
+app.use("/api/user", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(5000, (req, res) => {
+  connect();
+  console.log(`Server is listening on port 5000`);
+});
